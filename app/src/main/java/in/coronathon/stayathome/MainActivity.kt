892a3abbb04dea2757731b0cba77e7f79d373c26
Abhishek Batra <abhishek.batra@wingify.com>
2020-03-24 12:39:08 +0530

    first commit

---
+++ app/src/main/java/in/coronathon/stayathome/MainActivity.kt
@@ -0,0 +1,18 @@
+package `in`.coronathon.stayathome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import `in`.coronathon.stayathome.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
